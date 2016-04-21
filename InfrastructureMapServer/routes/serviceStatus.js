8a0453f1b3dfd72709b13e8793bf4f86ee4f3569
sameer <sameer.ranade@ymail.com>
2016-04-21 15:28:38 -0700

    added route for host status

--- InfrastructureMapServer/routes/serviceStatus.js
+++ InfrastructureMapServer/routes/serviceStatus.js
@@ -27,7 +27,72 @@ router.get('/', function (req, res, next) {
     console.log(NOVA_STATUS);
 
     var response = '{ "status" : {' + KEYSTONE_STATUS + ',' + GLANCE_STATUS + ',' + NEUTRON_STATUS + ',' + NOVA_STATUS + '}}';
-    //response = response.replace(/['"]+/g, '');
+    res.send(response);
+});
+
+
+router.get('/hosthealth', function (req, res, next) {
+    var CPU_CONTROLLER = "N/A";
+    var CPU_COMPUTE = "N/A";
+
+    var MEMORY_CONTROLLER = "N/A";
+    var MEMORY_COMPUTE = "N/A";
+
+    var DISK_READ_CONTROLLER = "N/A";
+    var DISK_READ_COMPUTE = "N/A";
+
+    var DISK_WRITE_CONTROLLER = "N/A";
+    var DISK_WRITE_COMPUTE = "N/A";
+
+    var CONTROLLER_HEALTH_STATUS = "Green";
+    var COMPUTE_HEALTH_STATUS = "Green";
+
+    CPU_CONTROLLER = fs.readFileSync('/var/log/ControllerHealth/CPUUsage.log');
+//console.log("CPU_CONTROLLER" + CPU_CONTROLLER);
+
+    MEMORY_CONTROLLER = fs.readFileSync('/var/log/ControllerHealth/MemoryUsage.log');
+//console.log("MEMORY_CONTROLLER" + MEMORY_CONTROLLER);
+
+    DISK_READ_CONTROLLER = fs.readFileSync('/var/log/ControllerHealth/DiskReadRate.log');
+//console.log("DISK_READ_CONTROLLER" + DISK_READ_CONTROLLER);
+
+    DISK_WRITE_CONTROLLER = fs.readFileSync('/var/log/ControllerHealth/DiskWriteRate.log');
+//console.log("DISK_WRITE_CONTROLLER" + DISK_WRITE_CONTROLLER);
+
+    CPU_COMPUTE = fs.readFileSync('/var/log/CPUUsage.log');
+//console.log("CPU_COMPUTE" + CPU_COMPUTE);
+
+    MEMORY_COMPUTE = fs.readFileSync('/var/log/MemoryUsage.log');
+//console.log("MEMORY_COMPUTE" + MEMORY_COMPUTE);
+
+    DISK_READ_COMPUTE = fs.readFileSync('/var/log/DiskReadRate.log');
+//console.log("DISK_READ_COMPUTE" + DISK_READ_COMPUTE);
+
+    DISK_WRITE_COMPUTE = fs.readFileSync('/var/log/DiskWriteRate.log');
+//console.log("DISK_WRITE_COMPUTE" + DISK_WRITE_COMPUTE);
+
+    if (parseInt(CPU_CONTROLLER) > 60 && parseInt(CPU_CONTROLLER) < 80) {
+        CONTROLLER_HEALTH_STATUS = "Orange";
+    }
+    else {
+        if (parseInt(CPU_CONTROLLER) >= 80) {
+            CONTROLLER_HEALTH_STATUS = "Red";
+        }
+    }
+
+    if (parseInt(CPU_COMPUTE) > 60 && parseInt(CPU_COMPUTE) < 80) {
+        COMPUTE_HEALTH_STATUS = "Orange";
+    }
+    else {
+        if (parseInt(CPU_COMPUTE) >= 80) {
+            COMPUTE_HEALTH_STATUS = "Red";
+        }
+    }
+
+    var response = '{ "health" : [' +
+        '{"HOST_NAME":"Controller", "CONTROLLER_HEALTH_STATUS":"' + CONTROLLER_HEALTH_STATUS + '", "CPU_CONTROLLER":"' + CPU_CONTROLLER + '%", "MEMORY_CONTROLLER":"' + MEMORY_CONTROLLER + 'Mb", "DISK_READ_CONTROLLER":"' + DISK_READ_CONTROLLER + 'Mb/s", "DISK_WRITE_CONTROLLER":"' + DISK_WRITE_CONTROLLER + 'Mb/s"},' +
+        '{"HOST_NAME":"Compute", "COMPUTE_HEALTH_STATUS":"' + COMPUTE_HEALTH_STATUS + '", "CPU_COMPUTE":"' + CPU_COMPUTE + '%", "MEMORY_COMPUTE":"' + MEMORY_COMPUTE + 'Mb", "DISK_READ_COMPUTE":"' + DISK_READ_COMPUTE + 'Mb/s", "DISK_WRITE_COMPUTE":"' + DISK_WRITE_COMPUTE + 'Mb/s"}]}';
+
     res.send(response);
 });
 
