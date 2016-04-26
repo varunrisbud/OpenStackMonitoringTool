9d116da99c1d34722e769157e66840a50ffad4dc
varunrisbud <varun.risbud@gmail.com>
2016-04-25 20:21:58 -0700

    Added endpoint to fetch the status of proactive workflow tests.

--- InfrastructureMapServer/routes/proactiveWorkFlowEndPoints.js
+++ InfrastructureMapServer/routes/proactiveWorkFlowEndPoints.js
@@ -7,6 +7,44 @@ var router = express.Router();
 var Client = require('node-rest-client').Client;
 var request = require('request');
 
+router.get('/status', function (req, res, data) {
+    var options = {
+        uri: 'http://130.65.159.58:9200/proactivewf/_search',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: {
+            "size" : 0,
+            "aggs" : {
+                "group" : {
+                    "terms" : {
+                        "field" : "message"
+                    },
+                    "aggs" : {
+                        "group_docs" : {
+                            "top_hits" : {
+                                "size" : 1,
+                                "sort" : [
+                                    {
+                                        "@timestamp" : {
+                                            "order" : "desc"
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    request.post(options, function (err, httpResponse, body) {
+        res.json(body);
+    });
+});
+
+
 router.get('/', function (req, res, data) {
     res.render('proactivemonitor');
 });
