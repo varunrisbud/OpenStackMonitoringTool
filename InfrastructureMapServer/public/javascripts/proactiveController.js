a82cbbe1764b5441bb4052de744cdeb2f34a42ab
varunrisbud <varun.risbud@gmail.com>
2016-04-26 02:34:20 -0700

    Added to refresh the test status after 65 secs.

--- InfrastructureMapServer/public/javascripts/proactiveController.js
+++ InfrastructureMapServer/public/javascripts/proactiveController.js
@@ -2,34 +2,47 @@
  * Created by Sa4w0c9 on 4/25/2016.
  */
 angular.module('opsmonitor', [])
-    .controller('proactiveController', function ($scope, $http) {
-        $http.get('http://130.65.159.170/proactiveTests/status').success(function (res) {
-            $scope.getGlyphiconCVM = function () {
-                if (res.aggregations.group.buckets[0].group_docs.hits.hits[0]._source.error == 'none') {
-                    return "glyphicon glyphicon-ok";
+    .controller('proactiveController', function ($scope, $http, $interval) {
+        var counter = 0;
+
+        $scope.getData = function () {
+            var createVMStatus = 'none';
+            var vmStatusStatus = 'none';
+            var deleteVMStatus = 'none';
+
+            $http.get('http://130.65.159.170/proactiveTests/status').success(function (res) {
+
+                $scope.getGlyphiconForTest = function(testName) {
+                    if(testName == 'createserver') return $scope.getGlyphicon(createVMStatus);
+
+                    else if(testName == 'vmstatus') return $scope.getGlyphicon(vmStatusStatus);
+
+                    else if(testName == 'deletevm') return $scope.getGlyphicon(deleteVMStatus);
                 }
-                else {
+
+                $scope.getGlyphicon = function(error) {
+                    if(error == 'none') return "glyphicon glyphicon-ok";
+
                     return "glyphicon glyphicon-remove";
                 }
-            }
 
-            $scope.getGlyphiconBVM = function () {
-                if (res.aggregations.group.buckets[2].group_docs.hits.hits[0]._source.error == 'none') {
-                    return "glyphicon glyphicon-ok";
-                }
-                else {
-                    return "glyphicon glyphicon-remove";
-                }
+                angular.forEach(res.aggregations.group.buckets, function(value, key) {
 
-            }
+                    if(value.key == 'createserver') createVMStatus = value.group_docs.hits.hits[0]._source.error;
 
-            $scope.getGlyphiconDVM = function () {
-                if (res.aggregations.group.buckets[1].group_docs.hits.hits[0]._source.error == 'none') {
-                    return "glyphicon glyphicon-ok";
-                }
-                else {
-                    return "glyphicon glyphicon-remove";
-                }
-            }
-        });
+                    else if(value.key == 'vmstatus') vmStatusStatus = value.group_docs.hits.hits[0]._source.error;
+
+                    else if (value.key == 'deletevm') deleteVMStatus = value.group_docs.hits.hits[0]._source.error;
+
+                    $scope.getGlyphiconForTest(value.key);
+                });
+
+            });
+            console.log(++counter);
+        };
+
+        $interval(function() {
+            console.log("inside Interval");
+            $scope.getData();
+        }, 65000);
     });
