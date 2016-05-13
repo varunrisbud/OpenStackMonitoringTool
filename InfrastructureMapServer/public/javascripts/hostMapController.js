364fc4afc73b789aeb5a806d547f8c4b3ddff573
sameer <sameer.ranade@ymail.com>
2016-05-13 13:18:00 -0700

    fixed hostmap related changes nad updated links

--- InfrastructureMapServer/public/javascripts/hostMapController.js
+++ InfrastructureMapServer/public/javascripts/hostMapController.js
@@ -6,24 +6,24 @@ angular.module('opsmonitor', [])
         $scope.myData = null;
         $http.get('http://130.65.159.170/status/hosthealth').success(function (res) {
             $scope.myData = res;
-            console.log("JS: " + res);
 
 
-            $scope.host_name = res.health[0].HOST_NAME;
-            $scope.controller_health_status = res.health[0].CONTROLLER_HEALTH_STATUS;
-            $scope.cpu_controller = res.health[0].CPU_CONTROLLER;
-            $scope.memory_controller = res.health[0].MEMORY_CONTROLLER;
-            $scope.disk_read_controller = res.health[0].DISK_READ_CONTROLLER;
-            $scope.disk_write_controller = res.health[0].DISK_WRITE_CONTROLLER;
-            console.log(res.health[0].HOST_NAME);
+            var cpuUsage = res[0].cpuUsage[0] + res[0].cpuUsage[1] + res[0].cpuUsage[2] + res[0].cpuUsage[3] + res[0].cpuUsage[4];
+            var memoryusage = /*TODO*/
 
-            $scope.host2_name = res.health[1].HOST_NAME;
-            $scope.compute_health_status = res.health[1].COMPUTE_HEALTH_STATUS;
-            $scope.cpu_compute = res.health[1].CPU_COMPUTE;
-            $scope.memory_compute = res.health[1].MEMORY_COMPUTE;
-            $scope.disk_read_compute = res.health[1].DISK_READ_COMPUTE;
-            $scope.disk_write_compute = res.health[1].DISK_WRITE_COMPUTE;
+            $scope.host_name = res[0].hostName;
+            $scope.cpu_controller = cpuUsage;
+            $scope.memory_controller = res[0].memoryUsage[0];
+            $scope.disk_read_controller = res[0].diskReadRate[0];
+            $scope.disk_write_controller = res[0].diskWriteRate[0];
+            //console.log(res.health[0].HOST_NAME);
+            //
+            //$scope.host2_name = res.health[1].HOST_NAME;
+            //$scope.compute_health_status = res.health[1].COMPUTE_HEALTH_STATUS;
+            //$scope.cpu_compute = res.health[1].CPU_COMPUTE;
+            //$scope.memory_compute = res.health[1].MEMORY_COMPUTE;
+            //$scope.disk_read_compute = res.health[1].DISK_READ_COMPUTE;
+            //$scope.disk_write_compute = res.health[1].DISK_WRITE_COMPUTE;
 
         });
-
     });
